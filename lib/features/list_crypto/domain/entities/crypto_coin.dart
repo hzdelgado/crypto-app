b579631eaf61563411786c965808098718cd37e9
Hillari Zorrilla <hillari.zorrilla@ext.blaze.me>
2022-10-23 22:48:02 -0500

    crypto coin prop current price not displaying fixed

--- lib/features/list_crypto/domain/entities/crypto_coin.dart
+++ lib/features/list_crypto/domain/entities/crypto_coin.dart
@@ -39,7 +39,7 @@ class CryptoCoin extends Equatable {
       'id': id,
       'name': name,
       'symbol': symbol,
-      'currentPrice': currentPrice,
+      'current_price': currentPrice,
       'starred': starred,
     };
   }
@@ -50,7 +50,7 @@ class CryptoCoin extends Equatable {
       id: map['id'] != null ? map['id'] as String : null,
       name: map['name'] != null ? map['name'] as String : null,
       symbol: map['symbol'] != null ? map['symbol'] as String : null,
-      currentPrice: map['currentPrice'] != null ? map['currentPrice'] as double : null,
+      currentPrice: map['current_price'] != null ? map['current_price'] as double : null,
       starred: map['starred'] != null ? map['starred'] as bool : false,
     );
   }
